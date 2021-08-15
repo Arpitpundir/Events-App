3da2f5bf4b25b2ea85ae34cbf05b96425242f1a6
Arpitpundir <thakurarpitpundir73@gmail.com>
2021-08-16 00:48:29 +0530

    Adding code for routing section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import {EventsListComponent} from "./events/events-list.component";
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent} from './events/event-thumbnail.component';
 import { NavbarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
+import { EventDetails } from './events/event-details/event-details.component';
+import { appRoutes } from './routes';
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavbarComponent
+    NavbarComponent,
+    EventDetails
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,15 +2,16 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'event-thumbnail',
-  template: `<div>
-    <div class="well hoverwell thumbnail">
+  template: `
+  <div>
+    <div [routerLink] = "['/events', event.id]" class="well hoverwell thumbnail">
       <h2>{{ event?.name }}</h2>
       <div>Date: {{ event?.date }}</div>
-      <div [ngClass] = "event.time === '8:00 am'" [ngSwitch]="event.time">
+      <div [ngClass] = "getStartTimeClass()" [ngSwitch]="event.time">
         Time: {{ event?.time }}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
-        <span *ngSwitchDefault="'8:00 am'">(Normal Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
       </div>
       <div>Price: {{ event?.price }}</div>
       <div>
@@ -36,4 +37,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+
+  getStartTimeClass(){
+    const isEarlyStart = this.event && this.event.time === '8:00 am';
+    return {'green': isEarlyStart, bold: isEarlyStart};
+  }
+
+  getStartTimeStyle(){
+    if(this.event && this.event.time === '8:00 am'){
+      return {
+        'color': '#003300',
+        'font-weight': 'bold'
+      }
+    }
+    return {};
+  }
 }
